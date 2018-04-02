710298768dd0449f69e5b0512102fa6517dec733
Graham Morehead <graham.morehead@gmail.com>
2018-04-02 12:09:59 -0400

    minor updates

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='gmutils',
-      version='0.0.2',
+      version='0.2.0',
       description='Python 3 utils to simplify natural language machine learning code',
       url='https://github.com/grahammorehead/gmutils.git',
       author='grahammorehead',
       license='GPL',
       packages=['gmutils'],
       install_requires=[
-          'spacy', 'pandas', 'numpy', 'scipy', 'pydot', 'matplotlib', 'elasticsearch', 'scikit-learn', 'tensorflow'
+          'spacy', 'pandas', 'numpy', 'scipy', 'pydot', 'matplotlib', 'elasticsearch', 'scikit-learn', 'tensorflow', 'keras'
       ],
       zip_safe=False)
