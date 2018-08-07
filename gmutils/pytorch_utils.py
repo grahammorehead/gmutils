78953afdfbcf4f2ad9e6959daae789f250ccb67a
Graham Morehead <gm@pangeon.com>
2018-08-07 15:57:39 -0700

    minor updates

--- gmutils/pytorch_utils.py
+++ gmutils/pytorch_utils.py
@@ -10,6 +10,7 @@ import inspect
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.nn.modules.loss as Loss
 import numpy as np
 from gmutils.utils import err, argparser, isTrue, read_dir
 
@@ -617,6 +618,54 @@ def pearson_loss(X, Y):
     """
     return 1.0 - pearson_coeff(X, Y)
     
+
+class PearsonLoss(Loss._Loss):
+    """
+    Creates a criterion that measures the Pearson Correlation Coefficient between labels and regressed output.
+
+    The sum operation still operates over all the elements, and divides by the batch size.
+
+    The division by `n` can be avoided if one sets the constructor argument
+    `size_average=False`.
+
+    Args:
+        size_average (bool, optional): Deprecated (see :attr:`reduction`). By default,
+            the losses are averaged over each loss element in the batch. Note that for
+            some losses, there multiple elements per sample. If the field :attr:`size_average`
+            is set to ``False``, the losses are instead summed for each minibatch. Ignored
+            when reduce is ``False``. Default: ``True``
+        reduce (bool, optional): Deprecated (see :attr:`reduction`). By default, the
+            losses are averaged or summed over observations for each minibatch depending
+            on :attr:`size_average`. When :attr:`reduce` is ``False``, returns a loss per
+            batch element instead and ignores :attr:`size_average`. Default: ``True``
+        reduction (string, optional): Specifies the reduction to apply to the output:
+            'none' | 'elementwise_mean' | 'sum'. 'none': no reduction will be applied,
+            'elementwise_mean': the sum of the output will be divided by the number of
+            elements in the output, 'sum': the output will be summed. Note: :attr:`size_average`
+            and :attr:`reduce` are in the process of being deprecated, and in the meantime,
+            specifying either of those two args will override :attr:`reduction`. Default: 'elementwise_mean'
+
+    Shape:
+        - Input: :math:`(N, *)` where `*` means, any number of additional
+          dimensions
+        - Target: :math:`(N, *)`, same shape as the input
+        - Output: scalar. If reduce is ``False``, then
+          :math:`(N, *)`, same shape as the input
+
+    Examples::
+
+        >>> loss = nn.PearsonLoss()
+        >>> input = torch.randn(3, 5, requires_grad=True)
+        >>> target = torch.randn(3, 5)
+        >>> output = loss(input, target)
+        >>> output.backward()
+    """
+    def __init__(self, size_average=None, reduce=None, reduction='elementwise_mean'):
+        super(PearsonLoss, self).__init__(size_average, reduce, reduction)
+
+    def forward(self, input, target):
+        return pearson_loss(input, target)
+
 
 ################################################################################
 # MAIN
